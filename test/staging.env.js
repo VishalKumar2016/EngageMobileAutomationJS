1430dfc55552252a974909483027cf0c9cabc328
Vishal Kumar <vishalkumar7458@gmail.com>
2020-01-28 17:47:08 +0530

    Add staging env file

---
+++ test/staging.env.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  appPackage: 'com.gaggleamp.engage.mobile.enterprise.staging',
+  app: '/Users/mac/Documents/Atom/Json/ReactNative/appium/Engage_Mobile_Maven/app-staging-debug.apk',
+};
